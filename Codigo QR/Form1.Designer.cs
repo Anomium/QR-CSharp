c068ddc5f785ea5359cba0e8a446d7d52a072a09
Anomium <melvin.martinez.97@gmail.com>
2018-08-01 15:35:43 -0500

    Codificando texto a QR

---
+++ Codigo QR/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Codigo_QR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Nombre = new System.Windows.Forms.RichTextBox();
+            this.pbg_Generar = new System.Windows.Forms.PictureBox();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbg_Generar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(24, 27);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(242, 150);
+            this.txt_Nombre.TabIndex = 2;
+            this.txt_Nombre.Text = "";
+            this.txt_Nombre.TextChanged += new System.EventHandler(this.txt_Nombre_TextChanged);
+            // 
+            // pbg_Generar
+            // 
+            this.pbg_Generar.Location = new System.Drawing.Point(24, 196);
+            this.pbg_Generar.Name = "pbg_Generar";
+            this.pbg_Generar.Size = new System.Drawing.Size(293, 288);
+            this.pbg_Generar.TabIndex = 3;
+            this.pbg_Generar.TabStop = false;
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.Location = new System.Drawing.Point(357, 167);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Guardar.TabIndex = 4;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 523);
+            this.Controls.Add(this.btn_Guardar);
+            this.Controls.Add(this.pbg_Generar);
+            this.Controls.Add(this.txt_Nombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbg_Generar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox txt_Nombre;
+        private System.Windows.Forms.PictureBox pbg_Generar;
+        private System.Windows.Forms.Button btn_Guardar;
+    }
+}
+
---
+++ Codigo QR/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace Codigo_QR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_Nombre_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_Nombre.Text != "")
+            {
+                //
+                BarcodeWriter br = new BarcodeWriter();
+                //
+                br.Format = BarcodeFormat.QR_CODE;
+                //
+                Bitmap bm = new Bitmap(br.Write(txt_Nombre.Text), 300, 300);
+                pbg_Generar.Image = bm;
+            }
+            
+        }
+    }
+}
