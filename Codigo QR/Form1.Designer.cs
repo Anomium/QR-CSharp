22248a8daa4e78b56978af8677fe87fb618fdfd8
Anomium <melvin.martinez.97@gmail.com>
2018-08-02 14:46:03 -0500

    Boton para guardar imagen completo

--- Codigo QR/Form1.Designer.cs
+++ Codigo QR/Form1.Designer.cs
@@ -31,12 +31,22 @@
             this.txt_Nombre = new System.Windows.Forms.RichTextBox();
             this.pbg_Generar = new System.Windows.Forms.PictureBox();
             this.btn_Guardar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pbg_codigo = new System.Windows.Forms.PictureBox();
+            this.btn_Abrir = new System.Windows.Forms.Button();
+            this.txt_Texto = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbg_Generar)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbg_codigo)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_Nombre
             // 
-            this.txt_Nombre.Location = new System.Drawing.Point(24, 27);
+            this.txt_Nombre.Location = new System.Drawing.Point(8, 20);
             this.txt_Nombre.Name = "txt_Nombre";
             this.txt_Nombre.Size = new System.Drawing.Size(242, 150);
             this.txt_Nombre.TabIndex = 2;
@@ -45,7 +55,7 @@
             // 
             // pbg_Generar
             // 
-            this.pbg_Generar.Location = new System.Drawing.Point(24, 196);
+            this.pbg_Generar.Location = new System.Drawing.Point(8, 189);
             this.pbg_Generar.Name = "pbg_Generar";
             this.pbg_Generar.Size = new System.Drawing.Size(293, 288);
             this.pbg_Generar.TabIndex = 3;
@@ -53,24 +63,94 @@
             // 
             // btn_Guardar
             // 
-            this.btn_Guardar.Location = new System.Drawing.Point(357, 167);
+            this.btn_Guardar.Location = new System.Drawing.Point(256, 148);
             this.btn_Guardar.Name = "btn_Guardar";
             this.btn_Guardar.Size = new System.Drawing.Size(75, 23);
             this.btn_Guardar.TabIndex = 4;
             this.btn_Guardar.Text = "Guardar";
             this.btn_Guardar.UseVisualStyleBackColor = true;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AccessibleName = "probando";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(383, 533);
+            this.tabControl1.TabIndex = 5;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pbg_Generar);
+            this.tabPage1.Controls.Add(this.btn_Guardar);
+            this.tabPage1.Controls.Add(this.txt_Nombre);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(375, 507);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Crear";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pbg_codigo);
+            this.tabPage2.Controls.Add(this.btn_Abrir);
+            this.tabPage2.Controls.Add(this.txt_Texto);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(375, 507);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Leer";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pbg_codigo
+            // 
+            this.pbg_codigo.Location = new System.Drawing.Point(26, 194);
+            this.pbg_codigo.Name = "pbg_codigo";
+            this.pbg_codigo.Size = new System.Drawing.Size(293, 288);
+            this.pbg_codigo.TabIndex = 6;
+            this.pbg_codigo.TabStop = false;
+            // 
+            // btn_Abrir
+            // 
+            this.btn_Abrir.Location = new System.Drawing.Point(274, 153);
+            this.btn_Abrir.Name = "btn_Abrir";
+            this.btn_Abrir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Abrir.TabIndex = 7;
+            this.btn_Abrir.Text = "Abrir";
+            this.btn_Abrir.UseVisualStyleBackColor = true;
+            this.btn_Abrir.Click += new System.EventHandler(this.btn_Abrir_Click);
+            // 
+            // txt_Texto
+            // 
+            this.txt_Texto.Location = new System.Drawing.Point(26, 25);
+            this.txt_Texto.Name = "txt_Texto";
+            this.txt_Texto.Size = new System.Drawing.Size(242, 150);
+            this.txt_Texto.TabIndex = 5;
+            this.txt_Texto.Text = "";
+            this.txt_Texto.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 523);
-            this.Controls.Add(this.btn_Guardar);
-            this.Controls.Add(this.pbg_Generar);
-            this.Controls.Add(this.txt_Nombre);
+            this.ClientSize = new System.Drawing.Size(382, 534);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbg_Generar)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbg_codigo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -79,6 +159,12 @@
         private System.Windows.Forms.RichTextBox txt_Nombre;
         private System.Windows.Forms.PictureBox pbg_Generar;
         private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pbg_codigo;
+        private System.Windows.Forms.Button btn_Abrir;
+        private System.Windows.Forms.RichTextBox txt_Texto;
     }
 }
 
--- Codigo QR/Form1.cs
+++ Codigo QR/Form1.cs
@@ -22,15 +22,48 @@ namespace Codigo_QR
         {
             if (txt_Nombre.Text != "")
             {
-                //
+               
                 BarcodeWriter br = new BarcodeWriter();
-                //
+                
                 br.Format = BarcodeFormat.QR_CODE;
-                //
+                
                 Bitmap bm = new Bitmap(br.Write(txt_Nombre.Text), 300, 300);
                 pbg_Generar.Image = bm;
             }
             
+        }
+
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Guardar = new SaveFileDialog();
+            if (Guardar.ShowDialog() == DialogResult.OK)
+            {
+                Guardar.Filter = "JPEG(*.JPG)|*.JPG|BMP(*.BMP)|*.BMP";
+                Image imagen = pbg_Generar.BackgroundImage;
+                
+                pbg_Generar.Image.Save(Guardar.FileName);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Abrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
         }
     }
 }
